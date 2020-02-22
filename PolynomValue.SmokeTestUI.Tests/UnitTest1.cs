fa5d4c79ad607fa31416265d2bffe9568d1ae037
AdilBikeev <bikeev1999@mail.ru>
2020-02-22 12:02:11 +0300

    develop-test Добавил проверяющий вычисление значение P(x)*Q(y)-P(Q(x+y))

--- PolynomValue.SmokeTestUI.Tests/UnitTest1.cs
+++ PolynomValue.SmokeTestUI.Tests/UnitTest1.cs
@@ -91,5 +91,45 @@ namespace PolynomValue.SmokeTestUI.Tests
                 Assert.AreEqual(expected, window.res);
             }
         }
+
+        [DataTestMethod]
+        [Description("Проверка корректной работы UI для вычисление значения P(x)*Q(y)-P(Q(x+y))")]
+        [DataRow("", "", 2, 2, 404)]
+        [DataRow(" ", "", 2, 2, 404)]
+        [DataRow(".", "", 2, 2, 404)]
+        [DataRow("/", "", 2, 2, 404)]
+        [DataRow("?", "", 2, 2, 404)]
+        [DataRow("$", "", 2, 2, 404)]
+        [DataRow("#", "", 2, 2, 404)]
+        [DataRow("!", "", 2, 2, 404)]
+        [DataRow("1", "1", 2, 2, 0)]
+        [DataRow("x+1", "1", 2, 2, 1)]
+        [DataRow("x2+x+1", "1", 2, 2, 4)]
+        [DataRow("x2+x+1", "1", -2, 2, 4)]
+        [DataRow("x3+x2+x+1", "1", 2, 25, 11)]
+        public void CalcSumPolynomTest(string expFirst, string expSecond, int operandFirst, int operandSecond, double expected)
+        {
+            MainWindow window = new MainWindow();
+
+            window.setPx(expFirst);
+            window.setX(operandFirst);
+            window.clickPx();
+            var firstPolynomRes = window.res;
+
+            window.setQy(expSecond);
+            window.setY(operandSecond);
+            window.clickQy();
+            var secondPolynomRes = window.res;
+
+            window.clickPolySumByXY();
+            if (expected == 404)
+            {
+                Assert.AreNotEqual(expected, window.res);
+            }
+            else
+            {
+                Assert.AreEqual(expected, window.res);
+            }
+        }
     }
 }
